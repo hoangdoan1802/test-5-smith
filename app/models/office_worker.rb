6cec5aff5768781a7efafcbb3166faed76c88068
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 11:38:33 +0700

    Valitate dob for office_worker model and spec

--- app/models/office_worker.rb
+++ app/models/office_worker.rb
@@ -1,3 +1,12 @@
 class OfficeWorker < User
-	validates :year_of_exp, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :year_of_exp, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :dob_18
+
+  def dob_18
+    return if dob.blank?
+    if dob + 20.years >= Date.today
+      errors.add(:dob, "can't be under 20")
+    end
+  end
 end
--- spec/models/office_worker_spec.rb
+++ spec/models/office_worker_spec.rb
@@ -4,4 +4,11 @@ describe OfficeWorker do
   context 'Validation' do
     it { is_expected.to validate_numericality_of(:year_of_exp).is_greater_than_or_equal_to(0) }
   end
+
+  describe :age do
+    let(:office_worker)   { create :user, dob: '18-02-1992' }
+    it "has the proper age" do
+      expect(office_worker.age(office_worker.dob)).to eql(23)
+    end
+  end
 end
