0de8de8c00dbafa581aa6969a1c25efb21bc8880
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 11:26:27 +0700

    Update user model with def age

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
   validates :dob, presence: true
+  
+  def age(dob)
+    today = Date.today
+    age = today.year - dob.year
+    age -= 1 if dob.strftime("%m%d").to_i > today.strftime("%m%d").to_i
+    age
+  end
 end
