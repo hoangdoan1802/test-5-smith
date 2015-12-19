79d132e85e4d4fe52b52b7d60c71de0e27599893
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 10:32:06 +0700

    Validate year_of_exp of office_worker model and spec

--- app/models/office_worker.rb
+++ app/models/office_worker.rb
@@ -1,3 +1,3 @@
 class OfficeWorker < User
-	validates :year_of_exp, presence: true
+	validates :year_of_exp, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- spec/models/office_worker_spec.rb
+++ spec/models/office_worker_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe OfficeWorker do
   context 'Validation' do
-    it { is_expected.to validate_presence_of :year_of_exp }
+    it { is_expected.to validate_numericality_of(:year_of_exp).is_greater_than_or_equal_to(0) }
   end
 end
