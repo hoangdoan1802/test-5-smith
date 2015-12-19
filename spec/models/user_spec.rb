f58565358bb6ada91a440765e94fb083eae30920
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 11:58:13 +0700

    Write test case for user model with show age

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,11 @@ describe User do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :dob }
   end
+
+  describe :age do
+    let(:user)   { create :user, dob: '18-02-1992' }
+    it "Show age" do
+      expect(user.age).to eql(23)
+    end
+  end
 end
