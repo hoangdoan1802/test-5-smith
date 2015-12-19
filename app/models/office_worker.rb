e0dcc6593f6a46ec4863e5540519de0a773d2707
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 12:36:57 +0700

    Change #dob_18 to #dob_20 to office_worker model

--- app/models/office_worker.rb
+++ app/models/office_worker.rb
@@ -1,9 +1,9 @@
 class OfficeWorker < User
   validates :year_of_exp, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
-  validate :dob_18
+  validate :dob_20
 
-  def dob_18
+  def dob_20
     return if dob.blank?
     if dob + 20.years >= Date.today
       errors.add(:dob, "can't be under 20")
