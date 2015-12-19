d42c762ace88147c86f341d84b6088591e450292
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 09:44:25 +0700

    Create students table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219023458) do
+ActiveRecord::Schema.define(version: 20151219024143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20151219023458) do
     t.string   "name"
     t.string   "address"
     t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.date     "dob"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
