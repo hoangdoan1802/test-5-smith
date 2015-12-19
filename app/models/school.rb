b29e5d6ac2412ba3ee62506a7abdf13fae528cd8
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 12:32:47 +0700

    Add references to school table and spec

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,7 @@
 class School < ActiveRecord::Base
   validates :name, presence: true
   validates :address, presence: true
+
+  has_many :schools_users, foreign_key: "user_id"
+	has_many :students, :through => :schools_users
 end
--- app/models/schools_user.rb
+++ app/models/schools_user.rb
@@ -1,4 +1,4 @@
 class SchoolsUser < ActiveRecord::Base
   belongs_to :student, foreign_key: "user_id"
-	belongs_to :school
+  belongs_to :school
 end
