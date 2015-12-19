734b398a89f42c2cee23fa7cef214f3239bf92b4
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 14:43:15 +0700

    Write test case for student model with #schoolmates

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -14,6 +14,14 @@ describe Student do
   end
 
   describe :schoolmates do
-    # let(:student) { create :user, dob: '18-02-1997' }
+    let(:university_school) { create :school, type: "University" }
+
+    it "has the proper age" do
+      university_school.students.create(name: "Theo", dob: '18-02-1990')
+      user = university_school.students.create(name: "Oliver", dob: '18-02-1991')
+      ap user.schoolmates, plain: true
+      expect(university_school.students.size).to eq(2)
+      expect(user.schoolmates.count).to eq(1)
+    end
   end
 end
