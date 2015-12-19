1585717cf9ed1cadddc4461097cf55c368e658d1
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 13:36:01 +0700

    Write test case for student with #schoolmates

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -12,4 +12,8 @@ describe Student do
     it { is_expected.to have_many :schools_users }
     it { is_expected.to have_many :schools }
   end
+
+  describe :schoolmates do
+    # let(:student) { create :user, dob: '18-02-1997' }
+  end
 end
