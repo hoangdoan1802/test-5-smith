1a382892b875f6a70b72f86da26a53463b4439d5
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 12:35:16 +0700

    Write test case for school model with association

--- spec/models/school_spec.rb
+++ spec/models/school_spec.rb
@@ -5,4 +5,9 @@ describe School do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :address }
   end
+
+  context 'Association' do
+    it { is_expected.to have_many :students }
+    it { is_expected.to have_many :schools_users }
+  end
 end
