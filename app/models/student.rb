3aa891965d69fc62a63b552e5cc4877178d442a0
smith@futureworkz.com <smith@futureworkz.com>
2015-12-19 13:33:15 +0700

    Update student model with #schoolmates

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,14 +1,20 @@
 class Student < User
-	include Seeds::Student
+  include Seeds::Student
   validate :dob_18
 
   has_many :schools_users, foreign_key: "user_id"
-	has_many :schools, :through => :schools_users
+  has_many :schools, :through => :schools_users
 
   def dob_18
     return if dob.blank?
     if dob + 18.years >= Date.today
       errors.add(:dob, "can't be under 18")
     end
+  end
+
+  def schoolmates
+    school_ids = self.schools.pluck(:id)
+    student_ids = SchoolsUser.where(school_id: school_ids).pluck('user_id')
+    Student.where(id: student_ids).where.not(id: id)
   end
 end
